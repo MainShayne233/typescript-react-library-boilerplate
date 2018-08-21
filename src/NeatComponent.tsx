83c3a88ac34d73a75ddf938d6b7dedd7fb99808f
MainShayne233 <shaynetremblay@hotmail.com>
2018-08-20 23:07:03 -0400

    Get testing working!!!

--- src/NeatComponent.tsx
+++ src/NeatComponent.tsx
@@ -1,10 +1,6 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
 
-interface NeatComponentProps {
-  neatMesssageProp: string,
-}
-
 const NeatComponent: React.SFC<{neatMessageProp: string}> = ({neatMessageProp} : { neatMessageProp: string}) => (
   <div>
     <p id="message">{neatMessageProp}</p>
--- test/index_test.tsx
+++ test/index_test.tsx
@@ -3,20 +3,16 @@ import * as React from 'react';
 import * as Enzyme from 'enzyme';
 import * as EnzymeAdapter from 'enzyme-adapter-react-16';
 import { expect } from 'chai';
+import NeatComponent from '../src/NeatComponent';
 
-it ('should fail', () => {
-  expect(true).to.equal(false);
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+describe('NeatComponent', () => {
+  it('should render the neatMessageProp', () => {
+    const message = 'This is a test message!';
+    const componentInstance = <NeatComponent neatMessageProp={message} />;
+    const mountedComponent = Enzyme.shallow(componentInstance);
+    const messageNode = mountedComponent.find('p');
+    expect(messageNode.text()).to.equal(message);
+  });
 });
-//import NeatComponent from '../src/NeatComponent';
-//
-//Enzyme.configure({ adapter: new EnzymeAdapter() });
-//
-//describe('NeatComponent', () => {
-//  it('should render the neatMessageProp', () => {
-//    const message = 'This is a test message!';
-//    const componentInstance = <NeatComponent neatMessageProp={message} />;
-//    const mountedComponent = Enzyme.shallow(componentInstance);
-//    const messageNode = mountedComponent.find('p');
-//    expect(messageNode.text()).to.equal(message);
-//  });
-//});
